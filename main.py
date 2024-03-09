906938eaad1fc2e271cfcc2e51233ed83be6b668
Mario Ruci <marioruci15@gmail.com>
2024-03-09 13:52:12 +0200

    feat: komment demo

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 def get_user_input():
-    """
-    is defined to receive a JSON payload containing a list of numbers, and if
-    present, returns that list; otherwise, it returns `None`.
-
-    Returns:
-        `object`.: a list of integers represented as JSON.
-        
-        	The returned value is either an array of numbers or `None`, depending on
-        whether a valid JSON object containing numbers was provided in the request
-        or not.
-        
-
-    """
     try:
         numbers = request.json['numbers']
         return numbers
@@ -23,17 +10,6 @@ def get_user_input():
         return None
 
 def process_input(numbers):
-    """
-    takes a list of integers as input, performs squaring operation on each element
-    in the list and returns the squared list as output.
-
-    Args:
-        numbers (list): 1-dimensional array or list of integers to be squared.
-
-    Returns:
-        list: a list of squared numbers.
-
-    """
     if numbers:
         squared_numbers = [num ** 2 for num in numbers]
         return squared_numbers
@@ -41,24 +17,6 @@ def process_input(numbers):
         return None
 
 def display_result(squared_numbers):
-    """
-    takes a list of numbers as input, squares them if they are not empty, and
-    returns them as a JSON object with a status code of either 200 (if successful)
-    or 400 (if an error occurs).
-
-    Args:
-        squared_numbers (int): list of numbers to be squared.
-
-    Returns:
-        JSON object.: a JSON object containing the squared numbers if provided,
-        and an error message otherwise.
-        
-        		- The output is a tuple containing two elements: a dictionary with
-        key-value pairs representing the squared numbers and an integer status
-        code indicating the result (200 for successful execution or 400 for error).
-        
-
-    """
     if squared_numbers:
         return jsonify({'squared_numbers': squared_numbers}), 200
     else:
@@ -66,13 +24,6 @@ def display_result(squared_numbers):
 
 @app.route('/square', methods=['POST'])
 def square_numbers():
-    """
-    processes user input and returns the resulting squared numbers after processing.
-
-    Returns:
-        int: a list of square numbers calculated from user input.
-
-    """
     numbers = get_user_input()
     squared_numbers = process_input(numbers)
     return display_result(squared_numbers)
